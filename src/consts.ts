25a775fd0f907e19150268d854e4b78117c0ca38
rojasaNera <arojas@nera-agro.com>
2024-01-28 16:03:47 -0300

    primer commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Adro is me';
+export const SITE_DESCRIPTION = 'Soy Adro un amante de la tecnologia, naturaleza y otras cosas :)';
